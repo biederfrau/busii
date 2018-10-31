e4ac5a0bc02e9c51bb69c6e74ad0019a3aa55f05
Sonja Biedermann <s.bdrmnn@gmail.com>
2018-10-31 22:06:55 +0100

    oops precision of dates oopsie woopsie

--- extract_any_w_timestamp.rb
+++ extract_any_w_timestamp.rb
@@ -10,7 +10,7 @@ thing = ARGV[1]
 
 loads = extract_and_clean_machine_receives(f).flatten.keep_if { |ev| ev[:name].end_with? thing }.sort_by { |ev| ev[:timestamp] }.reduce(Hash.new { |h, k| h[k] = [] }) do |acc, ev|
   axis = ev[:name].split('/')[1]
-  acc[axis] << [ev[:timestamp], ev[:value]]
+  acc[axis] << [ev[:timestamp].iso8601(3), ev[:value]]
 
   acc
 end
