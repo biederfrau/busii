e1f216ee9edbd0b9d817e5f1486ec6f181eff9d4
Sonja Biedermann <s.bdrmnn@gmail.com>
2018-12-11 16:04:50 +0100

    3 decimals because d3 cant parse 4 fuck

--- vis/scripts/timestamps.rb
+++ vis/scripts/timestamps.rb
@@ -8,4 +8,4 @@ file = ARGV.first
 p = File.basename file, '.xes.yaml'
 events = extract_and_clean_machine_receives(file).flatten.map { |e| e[:server_timestamp] }.sort.uniq
 
-File.write File.join('..', 'data', p, 'timestamps.json'), JSON::generate(events)
+File.write File.join('..', 'data', p, 'timestamps.json'), JSON::generate(events.map { |x| x.iso8601(3) })
